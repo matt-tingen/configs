46c9c8556ad66ca12e6fe91eadcf15cce98703fc
Matt Tingen <mtingen@imagequix.com>
2018-11-26 11:59:21 -0500

    Add autocomplete for git alias
    https://gist.github.com/JuggoPop/10706934

--- bash/setup.sh
+++ bash/setup.sh
@@ -1,6 +1,12 @@
 # Use Homebrew install path first.
 export PATH=/usr/local/bin:$PATH
 
+# Autocomplete
 [ -f $(brew --prefix)/etc/bash_completion ] && . $(brew --prefix)/etc/bash_completion
 
+# Autocomplete git alias
+source $config_dir/git-completion.bash
+__git_complete g __git_main
+
+# Variables
 base_config=$HOME/.bashrc
