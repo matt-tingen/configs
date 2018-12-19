417be0f397e80ababfad3af9fb411c1c28232479
Matt Tingen <mtingen@imagequix.com>
2018-12-19 10:17:47 -0500

    Fix trim
    Previous version removed all spaces. This version also collapses interior whitespace.
    
    https://unix.stackexchange.com/a/205854

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -12,6 +12,6 @@ alias localip="ipconfig getifaddr en0"
 alias ip="curl -s http://checkip.dyndns.org/ | sed 's/[a-zA-Z<>/ :]//g'"
 alias refreshConfig='source $base_config'
 alias config='code $config_dir'
-alias trim="tr -d '[:space:]'"
+alias trim="awk '{\$1=\$1};1'"
 alias g="git"
 alias c="calc"
