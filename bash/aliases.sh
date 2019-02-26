910e785983948749ff9044efc3e2c57331eb8348
Matt Tingen <mtingen@imagequix.com>
2019-02-26 14:22:38 -0500

    Add `strip`

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -13,5 +13,7 @@ alias ip="curl -s http://checkip.dyndns.org/ | sed 's/[a-zA-Z<>/ :]//g'"
 alias refreshConfig='source $base_config'
 alias config='code $config_dir'
 alias trim="awk '{\$1=\$1};1'"
+# Remove trailing new line
+alias strip="awk '{printf $1}'"
 alias g="git"
 alias c="calc"
