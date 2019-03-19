710db608f3c63947944550f9ee722050e0f23e8b
Matt Tingen <matt.tingen@gmail.com>
2019-03-19 13:28:53 -0400

    Increase max buffer for commands
    This could use `spawn` to feed lines to `parseStatus`, but it doesn't seem worth it.
    
    Fixes #3

--- prompt/exec.js
+++ prompt/exec.js
@@ -1,9 +1,11 @@
 const { promisify } = require('util');
 const _exec = promisify(require('child_process').exec);
 
+const MAX_BUFFER = 512 * 1024; // 512kB
+
 const exec = (command, trim = true) => {
   const maybeTrim = result => (trim ? result.trim() : result);
-  return _exec(command)
+  return _exec(command, { maxBuffer: MAX_BUFFER })
     .then(({ stdout }) => maybeTrim(stdout))
     .catch(({ stderr }) => {
       throw maybeTrim(stderr);
