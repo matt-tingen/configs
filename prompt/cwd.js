0cb4346b3f1ff235965fbad23ce05e39b3cbd8cd
Matt Tingen <matt.tingen@gmail.com>
2019-03-22 09:59:57 -0400

    Show spaces in cwd as faded underscores
    Since I use a space as the end of the prompt, it was a little confusing having the path show with spaces.

--- prompt/cwd.js
+++ prompt/cwd.js
@@ -1,15 +1,23 @@
 const path = require('path');
 const color = require('./color');
 const git = require('./git');
+const os = require('os');
 
-const formatter = color('cyan');
+const formatter = dir => {
+  const parts = dir.split(' ');
+
+  return parts.map(color('cyan')).join(color('gray')('_'));
+};
+
+const replaceUserDir = dir => dir.replace(os.homedir(), '~');
 
 const cwd = async abbreviate => {
+  const cwd = process.cwd();
+
   if (!abbreviate) {
-    return formatter('\\w');
+    return formatter(replaceUserDir(cwd));
   }
 
-  const cwd = process.cwd();
   const gitRoot = await git('root');
   // Show the git root and any lower directories.
   const abbreviated = path.relative(path.resolve(gitRoot, '..'), cwd);
