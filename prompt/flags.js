3c2ed4f50cb419d076ca1c667f587fe655444a04
Matt Tingen <matt.tingen@gmail.com>
2019-04-10 13:30:44 -0400

    Distinguish work tree and index changes
    The untracked flag (`?`) is converted to an `A` for the work tree since it being untracked is implied. The untracked flag being present for the index is an artifact of the parsed format. Since a file in the index cannot be untracked, that flag is ignored.
    
    I'm unclear on when the `!` flag occurs, but it may require a similar special case since it works like `?` in the parsed format.
    
    Fixes #5

--- prompt/flags.js
+++ prompt/flags.js
@@ -3,26 +3,34 @@ const color = require('./color');
 const count = require('./count');
 
 const flagColors = {
-  M: 'yellow',
-  A: 'green',
-  C: 'green',
-  R: 'green',
-  '?': 'green',
-  D: 'red',
-  U: 'purple',
-  '!': 'purple',
+  M: 'yellow', // Modified
+  A: 'green', // Added
+  C: 'green', // Copied
+  R: 'green', // Renamed
+  D: 'red', // Deleted
+  U: 'purple', // Updated but unmerged
+  '!': 'purple', // Ignored
 };
 
-const flags = ({ changes }) => {
-  // Favor the flag for the index over the one for the work tree.
-  const flags = changes.map(change => change.x || change.y);
-  const counts = count(flags);
+const formatChanges = changes => {
+  const counts = count(changes.filter(Boolean));
 
   const components = Object.entries(counts).map(([flag, count]) =>
-    color(flagColors[flag])(count + flag)
+    color(flagColors[flag])(count + flag),
   );
 
   return components.join(' ');
+};
+
+const getWorkTreeFlag = ({ y: flag }) => (flag === '?' ? 'A' : flag);
+const getIndexFlag = ({ x: flag }) => (flag === '?' ? null : flag);
+
+const flags = ({ changes }) => {
+  const workTreeFlags = formatChanges(changes.map(getWorkTreeFlag));
+  const indexFlags = formatChanges(changes.map(getIndexFlag));
+  const showSeparator = !!indexFlags;
+
+  return workTreeFlags + (showSeparator ? color('gray')('|') : '') + indexFlags;
 };
 
 module.exports = flags;
