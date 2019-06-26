514927d8a2ecea6c85456d304b842178d2ffca05
Matt Tingen <matt.tingen@gmail.com>
2019-06-26 09:54:56 -0400

    Add shorthand for vscode

--- bash/functions.sh
+++ bash/functions.sh
@@ -44,12 +44,20 @@ timestamp() {
 # Wrap git. On errors, print an additional line in red.
 # Based on https://stackoverflow.com/a/35904417.
 git(){
-    command git "$@"
-    local exitCode=$?
-		# Ignores most error codes because they can correspond to things like
-		# quitting `more` or cancelling `git cor`.
-    if [ $exitCode -eq 1 ]; then
-        printf "\033[0;31mERROR: git exited with code $exitCode\033[0m\n"
-        return $exitCode
-    fi
+	command git "$@"
+	local exitCode=$?
+	# Ignores most error codes because they can correspond to things like
+	# quitting `more` or cancelling `git cor`.
+	if [ $exitCode -eq 1 ]; then
+		printf "\033[0;31mERROR: git exited with code $exitCode\033[0m\n"
+		return $exitCode
+	fi
+}
+
+e(){
+	if [ -z "$1" ]; then
+		code .
+	else
+		command code "$@"
+	fi
 }
