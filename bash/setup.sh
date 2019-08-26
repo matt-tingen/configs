dee7679357b9368046b9d7711868d77ca353161c
Matt Tingen <matt.tingen@gmail.com>
2019-08-26 09:48:06 -0400

    Block core-js ads

--- bash/setup.sh
+++ bash/setup.sh
@@ -14,3 +14,6 @@ __git_complete g __git_main
 base_config=$HOME/.bashrc
 # http://man7.org/linux/man-pages/man3/strftime.3.html
 HISTTIMEFORMAT="%a %F %I:%M:%S %p - " # Thu 2019-05-16 02:44:14 PM
+
+# https://github.com/zloirock/core-js/issues/548#issuecomment-495388335
+export ADBLOCK="1"
