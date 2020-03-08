2d1069d4ae78643c9313e4d0fb0644ca67b4fb8a
Matt Tingen <matt.tingen@gmail.com>
2020-03-07 19:41:59 -0500

    Enhance history

--- shell/setup.sh
+++ shell/setup.sh
@@ -10,14 +10,25 @@ if [ -x "$(command -v brew)" ]; then
     export PATH=/usr/local/bin:$PATH
 fi
 
+
 # Variables
 base_config=$HOME/.zshrc
-
-# http://man7.org/linux/man-pages/man3/strftime.3.html
-HISTTIMEFORMAT="%a %F %I:%M:%S %p - " # Thu 2019-05-16 02:44:14 PM
 
 # https://github.com/zloirock/core-js/issues/548#issuecomment-495388335
 export ADBLOCK="1"
 
 # https://docs.brew.sh/Analytics#opting-out
 export HOMEBREW_NO_ANALYTICS=1
+
+# History
+# https://scriptingosx.com/2019/06/moving-to-zsh-part-3-shell-options/
+setopt EXTENDED_HISTORY
+setopt SHARE_HISTORY
+setopt APPEND_HISTORY
+setopt HIST_IGNORE_DUPS
+setopt HIST_REDUCE_BLANKS
+# http://man7.org/linux/man-pages/man3/strftime.3.html
+HISTTIMEFORMAT="%a %F %I:%M:%S %p - " # Thu 2019-05-16 02:44:14 PM
+HISTFILE=$HOME/.zsh_history
+SAVEHIST=5000
+HISTSIZE=2000
