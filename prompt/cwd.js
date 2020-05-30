c9ec21b2a77fd6209d9945985f86130ec685e936
Matt Tingen <matt.tingen@gmail.com>
2020-05-30 17:11:47 -0400

    Include path from dev folder to git root
    This improves the abbreviated cwd for paths such as
    `~/Development/foo/server` from `server` to `foo/server`.

--- prompt/cwd.js
+++ prompt/cwd.js
@@ -1,31 +1,46 @@
+const os = require('os');
 const path = require('path');
 const color = require('./color');
 const git = require('./git');
-const os = require('os');
 
 const HOME_DIR = os.homedir();
 
-const formatter = dir => {
+const formatter = (dir) => {
   const parts = dir.split(' ');
 
   return parts.map(color('cyan')).join(color('gray')('_'));
 };
 
-const replaceUserDir = dir =>
+const replaceUserDir = (dir) =>
   dir.startsWith(HOME_DIR) ? dir.replace(HOME_DIR, '~') : dir;
+
+const abbreviate = (dir, altRoot) => {
+  if (!altRoot) {
+    return null;
+  }
+
+  const relative = path.relative(altRoot, dir);
+  const isSubDir = !relative.startsWith('..') && !path.isAbsolute(relative);
+  return isSubDir ? relative : null;
+};
+
+const getGitRoot = async () => {
+  try {
+    const root = await git('root');
+    return root && path.relative(root, '..');
+  } catch {}
+  return null;
+};
 
 const cwd = async () => {
   const cwd = process.cwd();
 
-  try {
-    const gitRoot = await git('root');
-    // Show the git root and any lower directories.
-    const abbreviated = path.relative(path.resolve(gitRoot, '..'), cwd);
+  const abbreviated =
+    abbreviate(cwd, process.env.PROMPT_CWD_ALT_ROOT) ||
+    abbreviate(cwd, await getGitRoot()) ||
+    replaceUserDir(cwd);
 
-    return formatter(abbreviated);
-  } catch (ignore) {}
-
-  return formatter(replaceUserDir(cwd));
+  return formatter(abbreviated);
 };
 
 module.exports = cwd;
