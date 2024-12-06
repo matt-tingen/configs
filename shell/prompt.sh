d804d4f6b957015d1126cda129477e638acc6443
Matt Tingen <matt.tingen@gmail.com>
2024-12-06 15:34:13 -0500

    Use mise-en-place instead of nodenv

--- shell/prompt.sh
+++ shell/prompt.sh
@@ -2,12 +2,10 @@ export LSCOLORS=GxFxCxDxBxegedabagaced
 
 RPROMPT="%{%F{008}%}%D{%m/%d} %t"
 
-prompt_command_node=$(cd $config_dir && echo $(nodenv which node))
+prompt_command_node=$(cd $config_dir && echo $(which node))
 
 function prompt_command {
   if [ -d "$PWD" ]; then
-    # `NODENV_VERSION` cannot be used because of
-    # https://github.com/ouchxp/nodenv-nvmrc/issues/5
     export PS1=$(eval $prompt_command_node $config_dir/prompt)
   else
     export PS1="%F{red}ENOENT%f "
