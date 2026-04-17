3fde4c986078e474c9294c4b3d38bba248168c40
Matt Tingen <matt.tingen@gmail.com>
2026-04-17 15:09:47 -0400

    Allow deactivating promp

--- shell/prompt.sh
+++ shell/prompt.sh
@@ -4,8 +4,12 @@ RPROMPT="%{%F{008}%}%D{%m/%d} %t"
 
 prompt_command_node=$(cd $config_dir && echo $(which node))
 
+alias deactivatePrompt="export CUSTOM_PROMPT=0"
+
 function prompt_command {
-  if [ -d "$PWD" ]; then
+  if [ "$CUSTOM_PROMPT" = "0" ]; then
+    export PS1="$PWD "
+  elif [ -d "$PWD" ]; then
     export PS1=$(eval $prompt_command_node $config_dir/prompt)
   else
     export PS1="%F{red}ENOENT%f "
