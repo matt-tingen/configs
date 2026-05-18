ff1396a0632a1b675d46cffb82db1637bb30c228
Matt Tingen <matt.tingen@gmail.com>
2026-05-18 13:28:54 -0400

    add PROMPT_TYPE

--- shell/prompt.sh
+++ shell/prompt.sh
@@ -4,16 +4,21 @@ RPROMPT="%{%F{008}%}%D{%m/%d} %t"
 
 prompt_command_node=$(cd $config_dir && echo $(which node))
 
-alias deactivatePrompt="export CUSTOM_PROMPT=0"
+alias deactivatePrompt="export PROMPT_TYPE=bare"
 
 function prompt_command {
-  if [ "$CUSTOM_PROMPT" = "0" ]; then
-    export PS1="$PWD "
-  elif [ -d "$PWD" ]; then
-    export PS1=$(eval $prompt_command_node $config_dir/prompt)
-  else
-    export PS1="%F{red}ENOENT%f "
-  fi
+  case "$PROMPT_TYPE" in
+    node)
+      if [ -d "$PWD" ]; then
+        export PS1=$(eval $prompt_command_node $config_dir/prompt)
+      else
+        export PS1="%F{red}ENOENT%f "
+      fi
+      ;;
+    bare)
+      export PS1="$PWD "
+      ;;
+  esac
 }
 
 precmd_functions+=(prompt_command)
